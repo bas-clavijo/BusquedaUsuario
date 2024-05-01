1c344b53e097e7d7d70fd773c9d1045bc78d723a
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-01 16:35:14 -0400

    Configuracion controlador de usuarios

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class UsuariosController extends Controller
 {
-    //
+    public function index(){
+       return Usuario::all();
+    }
 }
