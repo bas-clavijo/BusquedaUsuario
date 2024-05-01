dd373199d2663c120d193213ece033449fe7af65
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-01 19:00:56 -0400

    Se agrega funcion actualizar

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -16,4 +16,23 @@ class UsuariosController extends Controller
         $respuesta = Usuario::create($inputs);
         return $respuesta;
     }
+
+    public function update(Request $request, $id){
+        $User = Usuario::find($id);
+        if(isset($User)){
+            $User->nombre = $request->nombre;
+            $User->correo = $request->correo;
+            $User->contraseña = $request->contraseña;
+            $User->foto = $request->foto;
+            $User->estado = $request->estado;
+
+            return $User->save();
+        }else{
+            return response()->json([
+                'error'=>true,
+                'mensaje'=>true, "No existe el usuario"
+
+            ]);
+        }
+    }
 }
