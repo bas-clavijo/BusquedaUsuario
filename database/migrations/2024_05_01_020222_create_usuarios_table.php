7581b8d516a3f3e98b9b627f812c86a8509d3ddf
bas-clavijo <bas.clavijo@duocuc.cl>
2024-04-30 23:39:20 -0400

    Se agregan datos a Usuario

--- database/migrations/2024_05_01_020222_create_usuarios_table.php
+++ database/migrations/2024_05_01_020222_create_usuarios_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string("nombre");
+            $table->string("correo")->unique();
+            $table->string("contraseña");
+            $table->string("foto")->nullable();
             $table->timestamps();
         });
     }
