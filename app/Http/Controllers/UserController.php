6975658f2942621700f2a6916da6b1177233377e
bas-clavijo <bas.clavijo@duocuc.cl>
2024-05-02 01:41:35 -0400

    Configuracion de User y se agregan funciones

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -11,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return User::all();
     }
 
     /**
@@ -19,7 +21,13 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $inputs = $request->input();
+        $inputs->password = Hash::make(trim($request->password));
+        $User = User::create($inputs);
+        return response()->json([
+            'data'=>$User,
+            'mensaje'=>true, "Guardado correctamente."
+        ]);
     }
 
     /**
@@ -27,7 +35,18 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $User = User::find($id);
+        if(isset($User)){
+            return response()->json([
+                'data'=>$User,
+                'mensaje'=>"Encontrado con exito."
+            ]);
+        }else{
+            return response()->json([
+                'error'=>true,
+                'mensaje'=>"No ha sido encontrado."
+            ]);
+        }
     }
 
     /**
@@ -35,7 +54,29 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $User = User::find($id);
+        if(isset($User)){
+            $User->name = $request->name;
+            $User->email = $request->email;
+            $User->password = Hash::make($request->password);
+
+            if($User->save()){
+                return response()->json([
+                    'data'=>$User,
+                    'mensaje'=>true, "Usuario encontrado y actualizado con exito."
+                ]); 
+            }else{
+                return response()->json([
+                    'error'=>true,
+                    'mensaje'=>true, "Usuario no encontrado y no actualizado."
+                ]);
+            }
+        }else{
+            return response()->json([
+                'error'=>true,
+                'mensaje'=>true, "No existe el usuario."
+            ]);
+        }
     }
 
     /**
@@ -43,6 +84,29 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $User = User::find($id);
+        if(isset($User)){
+            $res=User::destroy($id);
+            if($res){
+                return response()->json([
+                    'data'=>$User,
+                    'mensaje'=>"Eliminado con exito"
+                ]);
+            }else{
+                return response()->json([
+                    'data'=>$User,
+                    'mensaje'=>"No existe"
+                ]);
+            }
+            return response()->json([
+                'data'=>$User,
+                'mensaje'=>"Ha sido encontrado"
+            ]);
+        }else{
+            return response()->json([
+                'error'=>true,
+                'mensaje'=>"No existe"
+            ]);
+        }
     }
 }
